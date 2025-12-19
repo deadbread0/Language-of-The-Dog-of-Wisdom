685cadf778623e00c75fafdfb80f6fcc480bb166
deadbread0 <kirakaramisheva2007@gmail.com>
2025-12-19 11:01:43 +0300

    Delete input.h

--- input.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <cstdlib>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <assert.h>
-
-char* PutDataFromFileToBuffer();
-int GetSizeOfInputFile(const char* file_name);
