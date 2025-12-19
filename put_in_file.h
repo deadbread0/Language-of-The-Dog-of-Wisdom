514fab7769cdf7e2071e13413ca7895dd73be18f
deadbread0 <kirakaramisheva2007@gmail.com>
2025-12-19 11:00:21 +0300

    Delete put_in_file.h

--- put_in_file.h
+++
@@ -1,9 +0,0 @@
-#ifndef FOR_TYPES
-#include "types.h"
-#endif
-#include <assert.h>
-#include "tree_functions.h"
-void PutTreeInFile(node_t* node, FILE* filee);
-node_t* ReadNode(int* pos, char* buffer);
-char* ReturnDataOfNode(int* pos, char* buffer);
-
